f0ad6aa5a61c19561ddaa8b179f4e5f17a35316f
JunlingZhang <admin@16861.com>
2022-09-18 21:19:17 +0800

    Create octzc.sh

---
+++ octzc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+OS_NAME=$( cat /etc/os-release | grep ^NAME | cut -d'=' -f2 | sed 's/"//gI' )
+
+case "$OS_NAME" in
+    "CentOS Linux")
+        sudo yum install ntpdate ntp -y
+
+        sudo timedatectl set-timezone "Asia/Shanghai"
+        sudo ntpdate -u ntp.aliyun.com time.windows.com time.apple.com time.google.com time.cloudflare.com
+        sudo hwclock --systohc # 写入硬件
+
+        sudo systemctl enable ntpd
+        sudo systemctl start ntpd
+        ;;
+    "Ubuntu" | "Linux Mint")
+        sudo apt install ntpdate ntp -y
+
+        sudo timedatectl set-timezone "Asia/Shanghai"
+        sudo ntpdate -u ntp.aliyun.com time.windows.com time.apple.com time.google.com time.cloudflare.com
+        sudo hwclock --systohc # 写入硬件
+
+        sudo systemctl enable ntp
+        sudo systemctl start ntp
+        ;;
+esac
